032543d19a37b5066039a6494574b398554018a0
Fernando Gomez <fernando.gomez@keyrus.com>
2021-03-02 22:35:33 -0500

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,11 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:fagomezch/fagomezch.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+git push -f https://github.com/fagomezch/fagomezch.github.io master:gh-pages
 
 cd -
